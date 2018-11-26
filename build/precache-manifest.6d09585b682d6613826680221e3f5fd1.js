65f7e19cc82201d38b2caefe07cad9fa0bdfb07b
Ariel Hanna <Ariel.Hanna@doubleverify.com>
2018-11-26 12:23:37 +0200

    initial commit

---
+++ build/precache-manifest.6d09585b682d6613826680221e3f5fd1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "f6d27a3a19dd6072891b",
+    "url": "/static/js/main.f6d27a3a.chunk.js"
+  },
+  {
+    "revision": "2dd891c45704ef41f3d5",
+    "url": "/static/js/1.2dd891c4.chunk.js"
+  },
+  {
+    "revision": "f6d27a3a19dd6072891b",
+    "url": "/static/css/main.e60c1e3a.chunk.css"
+  },
+  {
+    "revision": "0f881be323b8698c6a6488e7b04ff114",
+    "url": "/index.html"
+  }
+];
